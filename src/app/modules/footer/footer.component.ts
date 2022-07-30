8546b6e0f0464e13aa90ed31708b0a042faabd74
Renán Pérez Balladares <64661058+ropbGitHub@users.noreply.github.com>
2022-07-30 13:20:30 -0500

    ...

--- src/app/modules/footer/footer.component.ts
+++ src/app/modules/footer/footer.component.ts
@@ -4,18 +4,25 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-
+  //#region Variables
+  listSection: string[] = [
+    'Nosotros',
+    'Términos',
+    'Condiciones',
+    'Aviso legal',
+  ];
   public isLoged: boolean;
+  //#endregion
+
   constructor(private router: ActivatedRoute) {
     this.isLoged = false;
-   }
+  }
 
   ngOnInit(): void {
     this.isLoged = this.router.snapshot.params['logged'];
     console.log(this.isLoged);
   }
-
 }
