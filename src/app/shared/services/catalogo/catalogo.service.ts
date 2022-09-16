3d685d21517eb19a0248cf7aad2e6870ff221fba
hlbv17 <helenlbv17@gmail.com>
2022-09-15 19:02:29 -0500

    .

--- src/app/shared/services/catalogo/catalogo.service.ts
+++ src/app/shared/services/catalogo/catalogo.service.ts
@@ -1,32 +1,76 @@
-import { Categoria } from '../../interfaces/categoria';
+import { Categoria } from '../../interfaces/book';
 import { Injectable } from '@angular/core';
 import { Book } from '../../interfaces/book';
-import { BehaviorSubject } from 'rxjs';
-import { mockDataCategoria } from 'src/assets/ts/MOCK_DATA_Libros';
-import { mockDataLibros } from 'src/assets/ts/MOCK_DATA_Libros';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class CatalogoService {
-  /*private newsItemSource = new BehaviorSubject<string>('default message');
-  currentNewsItem = this.newsItemSource.asObservable();*/
-
   private sourceItemLibros = new BehaviorSubject<string>('default message');
   currentItemLibros = this.sourceItemLibros.asObservable();
 
   categoria = 0;
   agotado = false;
 
-  listCategorias: Categoria[] = mockDataCategoria;
+  listCategorias: Categoria[] = [];
 
-  listLibros: Book[] = mockDataLibros;
+  listLibros: Book[] = [];
 
-  constructor(private router: Router) {}
+  API_LIBROS: string = 'https://localhost:7257/api/Libros/';
 
-  getLibro() {
-    return this.listLibros.slice();
+  API_CATEGORIAS: string = 'https://localhost:7257/GetCategorias/';
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  /*getLibros() {
+    let auth_token = localStorage.getItem('token_value');
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `bearer ${auth_token}`,
+    });
+
+    return this.http.get(this.API_SERVER, { headers: header });
+  }*/
+
+  getLibros(): Observable<any> {
+    return this.http.get(this.API_LIBROS);
   }
+
+  getCategorias(): Observable<any> {
+    return this.http.get(this.API_CATEGORIAS);
+  }
+  /**************************/
+  getLibro() {
+    this.getLibros().subscribe(
+      (data) => {
+        console.log(data);
+        this.listLibros = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    //return this.listLibros.slice();
+  }
+
+  getCategoria() {
+    this.getCategorias().subscribe(
+      (data) => {
+        console.log(data);
+        this.listCategorias = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  listarCategorias() {
+    return this.listCategorias;
+  }
+
   agregarLibro(data: Book) {
     this.listLibros.unshift(data);
   }
@@ -50,32 +94,24 @@ export class CatalogoService {
     this.sourceItemLibros.next(newsItem);
   }
 
-  filtrarCategoria(
-    listLibros: Book[],
-    listCategoria: Categoria[],
-    categoria: number
-  ) {
-    var id = listCategoria.find((categ) => categ.id_categoria == categoria);
-    var libros = listLibros.filter(
-      (libro) => libro.id_categoria === id?.id_categoria
+  filtrarCategoria(categoria: number) {
+    var id = this.listCategorias.find(
+      (categ) => categ.id_categoria == categoria
     );
-
-    if (categoria > 0) {
-      libros;
-    } else if (categoria == 0) {
-      var libros = listLibros;
-    }
-    //var resultado = this.categoria >= 0 ? libros : listLibros;
-    console.log(categoria);
-    console.log(id);
-    console.log(libros);
-    /*
-    var lib = listLibros.filter((libro) => {
-      if (libro.id_categoria === id?.id_categoria) {
-        return libro;
+    for (let i = 0; i < 6; i++) {
+      var libros = this.listLibros.filter(
+        (libro) => libro.categoria[i].id_categoria === id?.id_categoria
+      );
+      if (categoria > 0) {
+        libros;
+      } else if (categoria == 0) {
+        var libros = this.listLibros;
       }
-    });*/
-    return libros;
+      console.log(categoria);
+      console.log(id);
+      console.log(libros);
+      return libros;
+    }
   }
 
   mostrarCategoria(id: number) {
